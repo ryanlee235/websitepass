1b6099c55ce79a9c4843ebf2237280eb7a86019d
ryanlee235 <67621248+ryanlee235@users.noreply.github.com>
2021-08-08 20:15:57 -0500

    Add files via upload

---
+++ password/webgen/web/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask, Blueprint
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] ='jfadsl;ksdapoi'
+    from .views import views 
+    from .auth import auth
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    return app
