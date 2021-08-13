ebad097a8ad38ad109fcc494224e7162fd62aeb3
ryanlee235 <67621248+ryanlee235@users.noreply.github.com>
2021-08-12 18:16:05 -0600

    Update views.py

--- password/webgen/web/views.py
+++ password/webgen/web/views.py
@@ -1,10 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,session
+from flask_login import login_required, current_user
 
 views = Blueprint('views',__name__)
+@views.route('/')
 @views.route('/home')
-def home():
-    return render_template('home.html')
 
-@views.route('/user')
-def user():
-    return render_template('userinfo.html')
+def home():
+    return render_template('user.html')
+
+
