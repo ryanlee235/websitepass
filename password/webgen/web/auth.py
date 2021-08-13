4d35b47182507bb8ef459bca2def27d56c923a5a
ryanlee235 <67621248+ryanlee235@users.noreply.github.com>
2021-08-12 18:15:42 -0600

    Update auth.py

--- password/webgen/web/auth.py
+++ password/webgen/web/auth.py
@@ -1,13 +1,25 @@
-from flask import Blueprint
+from flask import Blueprint, request, session, url_for, flash
+from flask.templating import render_template
+from werkzeug.utils import redirect
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth', __name__)
 
-
-@auth.route('/login')
+#defining the url for the page, and the methods
+@auth.route('/login', methods=['POST', "GET"])
 def login():
-    pass
+    #if there is data being sent from the website
+    if request.method =="POST":
+        #get the email
+        email = request.form.get("email")
+        #get the password
+        password = request.form.get("password")
 
-@auth.route('/newuser')
+
+
+
+@auth.route('/sign-up')
 def create_user():
-    pass
+    return render_template("newuser.html")
 
