2b9dbaab68459b4e3a74d0d5583ecd2dc8c9ee37
ryanlee235 <67621248+ryanlee235@users.noreply.github.com>
2021-08-16 14:02:26 -0600

    Update auth.py

--- password/webgen/web/auth.py
+++ password/webgen/web/auth.py
@@ -9,7 +9,7 @@ from flask_login import login_user,logout_user,login_required, current_user
 
 auth = Blueprint('auth', __name__)
 
-@auth.route("/login")
+@auth.route("/login", methods = ["GET", 'POST'])
 def login():
     if request.method == "POST":
 
@@ -19,7 +19,7 @@ def login():
         user = User.query.filter_by(email=email)
 
         if user:
-            if check_password_hash(user.password,password):
+            if check_password_hash(user.Passwords,password):
                 flash("login successful!", category='success')
                 login_user(user,remember=True)
                 return redirect(url_for("home.html"))
@@ -31,35 +31,37 @@ def login():
         
 
 
-@auth.route("/newuser")
+@auth.route("/newuser", methods = ["GET", "POST"])
 def new_user():
-    email = request.form.get("email")
-    username = request.form.get("username")
-    password1 = request.form.get('password1')
-    password2=request.form.get("password2")
 
-    email_exist = User.query.filter_by(email=email)
-    username_exist = User.query.filter_by(username=username)
+    if request.method == "POST":
+        email = request.form.get("email")
+        username = request.form.get("username")
+        password1 = request.form.get('password1')
+        password2=request.form.get("password2")
 
-    if email_exist:
-        flash("email already exist! ", category="error")
-    elif username_exist:
-        flash("username already exist! ", category='error')
-    elif password1 != password2:
-        flash("password does not match!",category='error')
-    elif password1 < 6:
-        flash("password not long enough!", category='error')
-    elif email < 4:
-        flash("email is not vaild!", category="error")
-    elif username < 2:
-        flash("username not long enough!", category="error")
+        email_exist = User.query.filter_by(email=email).first()
+        username_exist = User.query.filter_by(username=username).first()
 
-    else:
-        new_user = User(emal= email, username= username, password= generate_password_hash(password1 ,method="sha256"))
+        if email_exist:
+            flash("email already exist! ", category="error")
+        elif username_exist:
+            flash("username already exist! ", category='error')
+        elif password1 != password2:
+            flash("password does not match!",category='error')
+        elif password1 < 6:
+            flash("password not long enough!", category='error')
+        elif email < 4:
+            flash("email is not vaild!", category="error")
+        elif username < 2:
+            flash("username not long enough!", category="error")
 
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for("home.html"))
+        else:
+            new_user = User(emal= email, username= username, password= generate_password_hash(password1 ,method="sha256"))
+
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for("home.html"))
 
     return render_template("newuser.html", user = current_user)
 
