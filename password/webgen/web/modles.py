a168cfe02636c515769ee889019895aa4855225e
ryanlee235 <67621248+ryanlee235@users.noreply.github.com>
2021-08-16 14:03:04 -0600

    Update modles.py

--- password/webgen/web/modles.py
+++ password/webgen/web/modles.py
@@ -3,15 +3,15 @@ from sqlalchemy.sql import func
 
 class User(db.Model):
     #every single row needs to have a different id, primary key makes sure there is only one kind of data
-    _id = db.Columm("id",db.Integer, primary_key=True)
-    email = db.Column("email", db.String(100))
-    user_name= db.Column("user",db.String(100))
-    password = db.Column("password",db.String(100))
+    _id = db.Column(db.Integer, primary_key=True)
+    email = db.Column( db.String(100), unique =True)
+    username= db.Column(db.String(100), unique =True)
+    Passwords = db.Column(db.String(100))
 
 
 
 class Passwords(db.Model):
     id = db.Column("id", db.Integer, primary_key=True)
     website = db.Column("website", db.String(100))
-    password = db.Column("Password1", db.string(200))
+    password = db.Column("Password1", db.String(200))
 
