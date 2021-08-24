230cb2c77b5bd671cd06ef304c82ca5b94e02de6
ryanlee235 <67621248+ryanlee235@users.noreply.github.com>
2021-08-24 11:58:56 -0600

    Update views.py

--- password/webgen/web/views.py
+++ password/webgen/web/views.py
@@ -1,11 +1,21 @@
 from flask import Blueprint, render_template,session
 from flask_login import login_required, current_user
+from .models import User, Passwords 
 
 views = Blueprint('views',__name__)
 @views.route('/')
 @views.route('/home')
-
+@login_required
 def home():
+    return Passwords.query.all()
     return render_template('user.html')
 
+@login_required
+#going to take the user input and create password, and also take the users website
+def create_password():
+    pass
 
+@login_required
+#this will just delete the password.
+def delete_password():
+    pass
