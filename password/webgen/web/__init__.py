46685d005fa2026b991cb588d384e8059e963f68
ryanlee235 <67621248+ryanlee235@users.noreply.github.com>
2021-08-24 11:58:30 -0600

    Update __init__.py

--- password/webgen/web/__init__.py
+++ password/webgen/web/__init__.py
@@ -2,39 +2,37 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
-
+# naming a variable and created the sqlalchmey
 db = SQLAlchemy()
-DB_NAME = "database.db"
+# setting the name to the database
+DB_NAME = 'database.db'
 
-
+#this is our main function that makes the whole app run
 def create_app():
+    #creates the flask app
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = "helloworld"
+    #this configures a secret key to make our website more secure
+    app.config['SECRET_KEY'] = 'battlefield2042'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    #initialzing the database 
     db.init_app(app)
-
+    #importing the blueprints
+    from .auth import auth 
     from .views import views
-    from .auth import auth
-
-    app.register_blueprint(views, url_prefix="/")
-    app.register_blueprint(auth, url_prefix="/")
-
-    from .models import User
-
+    #need to set up the blueprints so we can run these in the app
+    app.register_blueprint(auth)
+    app.register_blueprint(views)
+    from .models import User, Passwords
+    #calling the create_database function 
     create_database(app)
-
-    login_manager = LoginManager()
-    login_manager.login_view = "auth.login"
-    login_manager.init_app(app)
-
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
 
     return app
 
 
+#creates the database
 def create_database(app):
+    #if there is not a file in the website file path named database.db
     if not path.exists("website/" + DB_NAME):
+        #create the data base
         db.create_all(app=app)
-        print("Created database!")
+        print('created database!')
