d37592b5936885c9bc8274ea25eb83d349aab5b6
ryanlee235 <67621248+ryanlee235@users.noreply.github.com>
2021-09-04 20:58:14 -0500

    Update views.py

--- password/webgen/web/views.py
+++ password/webgen/web/views.py
@@ -1,21 +1,73 @@
-from flask import Blueprint, render_template,session
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
-from .models import User, Passwords 
-
+from .models import User, Passwords
+from. import db
+import random 
 views = Blueprint('views',__name__)
 @views.route('/')
 @views.route('/home')
-@login_required
+# home will display our user info, this will also  display the passwords for the user and website they are using for the passwords
 def home():
-    return Passwords.query.all()
-    return render_template('user.html')
+    pw = Passwords.query.all()
+    return render_template('home.html', user= current_user, password=pw)
 
-@login_required
-#going to take the user input and create password, and also take the users website
-def create_password():
-    pass
 
-@login_required
-#this will just delete the password.
-def delete_password():
-    pass
+@views.route('/create-password',methods = ['GET','POST'])
+def generate_password(length,upper,lower,nums,syms):
+    if request.method == "POST":
+        pw = request.form.get()
+
+        password_exist = Passwords.query.filter_by(password = pw)
+
+        if password_exist:
+            flash("password already exist!", category='error')
+        else:
+            flash("password does not exist!", category="error")
+    
+        upper_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        lower_letters = upper_letters.lower()
+        symbols ='!@#$%^&*()?<>/:;'
+        num = '12345678'
+
+        all=""
+        if upper:
+            all+= upper_letters
+        if lower:
+            all += lower_letters
+        if nums:
+            all+= num
+        if syms:
+            all+= symbols 
+
+            
+        amount = 1
+            
+
+        for x in range(amount):
+            password = ''.join(random.sample(all,length))
+            
+            new_password = Passwords(Passwords=password)
+            
+            db.session.add(new_password)
+            db.session.commit()
+            flash("password successfully added!")
+
+
+@views.route('/delete-password/<id>')
+def delete():
+    password = Passwords.query.filter_by(id=id).first()
+    
+    if not password:
+        flash("password does not exist! ", category ="error")
+    elif current_user.id != password.id:
+        flash("you do not have permission to delete this password", category='error')
+    else:
+        db.session.delete(password)
+        db.session.commit()
+        flash("password successfully deleted! ")
+
+@views.route('/website-name/<website>', methods = ['GET', 'POST'])
+def website():
+    if request.method == "POST":
+        website  = request.form.get()
+        website_exist = Passwords.query.filter_by()
