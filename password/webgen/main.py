57cd4c2fa8270809c5defe790db332edcea0e61d
ryanlee235 <67621248+ryanlee235@users.noreply.github.com>
2022-04-08 14:13:17 -0500

    Update main.py

--- password/webgen/main.py
+++ password/webgen/main.py
@@ -61,10 +61,36 @@ def microcenter(search_term):
         print(items)
         print(price_tag)
 
+def amazon(search_term):
+    amazon_url = f"https://www.amazon.com/s?k={search_term}&crid=1QHGKAI1TNU4N&sprefix=3080%2Caps%2C437&ref=nb_sb_noss_1"
+    amazon_page = requests.get(amazon_url).text
+    ba = BeautifulSoup(amazon_page, "html.parser")
+    
+    div = ba.find(class_="sg-col-inner")
+    items = div.find_all(text=re.compile(search_term))
+
+
+    for item in items:
+        parent = parent.item
+        link = None
+
+        if parent != "a":
+            continue
+        
+        link = parent["href"]
+
+
+    #need to find the price later
+
+
+    print(ba)
+
+
 def main():
     search_term = input("enter the item you are looking for: ")
-    new_egg(search_term)
-    microcenter(search_term)
+    #new_egg(search_term)
+    #microcenter(search_term)
+    amazon(search_term)
     
 
 if __name__ == '__main__':
