a2022d3f106903e032b41d62a3f0a1ce22765e20
Ryan Lee <ryanlee841@gmail.com>
2022-05-17 21:11:17 -0500

    found bug that would not allow us to log in or create user

--- password/webgen/web/auth.py
+++ password/webgen/web/auth.py
@@ -1,90 +1,69 @@
-from flask import Blueprint, request, session, url_for, flash
-from flask.templating import render_template
-from sqlalchemy.sql.functions import user
-from werkzeug.utils import redirect
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Blueprint, render_template, redirect, url_for, request, flash
 from . import db
 from .models import User
-from flask_login import login_user,logout_user,login_required, current_user
-#setting up blueprint
-auth = Blueprint('auth', __name__)
-#making a url with http methods.
-@auth.route("/login", methods = ["GET", 'POST'])
+from flask_login import login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
+auth = Blueprint("auth", __name__)
+
+
+@auth.route("/login", methods=['GET', 'POST'])
 def login():
-    # if the website is sending information
-    if request.method == "POST":
-        #grabe the email from the website
+    if request.method == 'POST':
         email = request.form.get("email")
-        #grab the password from the website
         password = request.form.get("password")
 
-        # we are looking in the database under the column email, and grabbing the first one 
         user = User.query.filter_by(email=email).first()
-
         if user:
-            #checking the password with the hash method
-            if check_password_hash(user.Passwords, password):
-                #tell the user they have been logged in 
-                flash("Succefully logged in!", category="success")
-                login_user(user,remember=True)
-                #gets redirected to the home page/ user page
-                return redirect("/home")
+            if check_password_hash(user.password, password):
+                flash("Logged in!", category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
             else:
-                flash("email or password incorrect!",category ='error')
-
+                flash('Password is incorrect.', category='error')
         else:
-            flash("email does not exist!", category="error")
+            flash('Email does not exist.', category='error')
 
-    return render_template("login.html",user =current_user)
+    return render_template("login.html", user=current_user)
 
-   
-@auth.route("/newuser", methods = ["GET", "POST"])
-def new_user():
-    if request.method =="POST":
+
+@auth.route("/newuser", methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
         email = request.form.get("email")
-        user = request.form.get("username")
+        username = request.form.get("username")
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
 
+        email_exists = User.query.filter_by(email=email).first()
+        username_exists = User.query.filter_by(username=username).first()
 
-        email_exist = User.query.filter_by(email=email).first()
-        user_exist = User.query.filter_by(username=user).first()
-
-        if email_exist:
-            flash("email already exist!",category='error')
-        elif user_exist:
-            flash("username is taken!",category="error")
+        if email_exists:
+            flash('Email is already in use.', category='error')
+        elif username_exists:
+            flash('Username is already in use.', category='error')
         elif password1 != password2:
-            flash("Password does not match!",category="error")
-        elif len(email) < 6:
-            flash("email not valid", category="error")
-        elif len(password1) < 8:
-            flash("password is not long enough", category="error")
-        elif len(user) < 6:
-            flash("username is not long enough! ",category='error')
-        
+            flash('Password don\'t match!', category='error')
+        elif len(username) < 2:
+            flash('Username is too short.', category='error')
+        elif len(password1) < 6:
+            flash('Password is too short.', category='error')
+        elif len(email) < 4:
+            flash("Email is invalid.", category='error')
         else:
-            #taking all the information and storing it in a variable called new_user
-            new_user = User(email=email, username=user, Passwords=generate_password_hash(
+            new_user = User(email=email, username=username, password=generate_password_hash(
                 password1, method='sha256'))
-            #going to add all this information to the database
             db.session.add(new_user)
-            #have to commit the change so the change takes place
             db.session.commit()
-            login_user(email_exist,remember=True)
-            flash("account has been created!",category="success")
+            login_user(new_user, remember=True)
+            flash('User created!')
+            return redirect(url_for('views.home'))
 
-            
-    
-    return render_template("newuser.html",user= current_user)
+    return render_template("newuser.html", user=current_user)
 
 
 @auth.route("/logout")
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for("auth.login"))
-
-
-
-
+    return redirect(url_for("views.home"))
--- password/webgen/web/models.py
+++ password/webgen/web/models.py
@@ -3,19 +3,17 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 class User(db.Model, UserMixin):
-    #every single row needs to have a different id, primary key makes sure there is only one kind of data
-    _id = db.Column(db.Integer, primary_key=True)
-    email = db.Column( db.String(100), unique =True)
-    username= db.Column(db.String(100), unique =True)
-    Passwords = db.Column(db.String(100))
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    username = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
     
-
 
 
 class Passwords(db.Model):
     id = db.Column("id", db.Integer, primary_key=True)
     website = db.Column("website", db.String(100))
-    password = db.Column("Password1", db.String(200))
+    user_passwords = db.Column("Password1", db.String(200))
 
     
 
